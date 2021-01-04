898b53e809387bc537d49052600185ccd5777e39
Igor <ihor.prots.liv@gmail.com>
2021-01-04 15:46:42 +0200

    7-10 from Igor

--- Lab3/main/views.py
+++ Lab3/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+import os
+def MyView(request):
+    return render(request, 'main.html',{'selector':'value'})
+def MyJson(request):
+    resault={
+            'word1':'value1',
+            'word2':'value2',
+            'word3':'value3',
+            'word4':'value4',
+            'word5':'value5',
+            }
+    return JsonResponse(resault)
